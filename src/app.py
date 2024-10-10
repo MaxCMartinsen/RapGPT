47ce48c81b17ce9617aa636e2ae59a155483950c
aniqsuperman <aniqsabir0@gmail.com>
2024-10-10 14:21:19 +0200

    hallo

--- src/app.py
+++ src/app.py
@@ -19,10 +19,33 @@ def save_audio_route():
     cc.generate_lyrics()
     return f"Saved to {audio_path}"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+from flask import Flask, request, render_template, send_from_directory
+from audio_handler import save_audio
+import ChatClient as cc
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/save', methods=['POST'])
+def save_audio_route():
+    if 'audio' not in request.files:
+        return "No audio file provided", 400
+
+    audio_file = request.files['audio']
+    audio_path = save_audio(audio_file)
+    cc.generate_lyrics()
+    return f"Saved to {audio_path}"
 
 
 @app.route('/uploads/<path:filename>')
 def serve_audio(filename):
+    print("don")
     return send_from_directory('uploads', filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
