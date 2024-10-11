6d6fe686f5d509924a5ce63021c675f84b2c3b07
aniqsuperman <aniqsabir0@gmail.com>
2024-10-11 10:12:46 +0200

    awdawd

--- src/ChatClient.py
+++ src/ChatClient.py
@@ -10,7 +10,7 @@ def generate_lyrics():
     client = OpenAI(api_key=apikey)
     
     # Define paths
-    audio_file_path = Path("./uploads/recording.mp3")
+    audio_file_path = Path("./src/static/uploads/recording.mp3")
     speech_file_path = Path(__file__).parent / "speech.mp3"
 
     try:
@@ -28,23 +28,15 @@ def generate_lyrics():
             model="gpt-4o",
             messages=[
                 {"role": "system", "content": text},
-<<<<<<< HEAD
-                {"role": "user", "content": "Answer the sentence in rap"}
-=======
                 {"role": "user", "content": "Answer the sentence in rap; make a description (max 200 charaters) for a beat that you would like to use for the rap; and response in json, lyrics called lyrics, and description called description."}
->>>>>>> 9ab3829fb4462ae3faee7e944c0e176391532bd8
             ]
         )
         lyrics = completion.choices[0].message.content
-        lyrics = json.loads["lyrics"]
         print(lyrics)
-<<<<<<< HEAD
-=======
         # description = json.loads(completion.choices[0].message.content)
         # description = description["description"]
         # lyrics = description["lyrics"]
         # print(description)
->>>>>>> 9ab3829fb4462ae3faee7e944c0e176391532bd8
 
         # Generate and save speech
         response = client.audio.speech.create(
--- src/Voice_handler.py
+++ src/Voice_handler.py
@@ -3,21 +3,22 @@ from elevenlabs import play
 import elevenlabs
 from moviepy.editor import concatenate_audioclips, AudioFileClip
 from pydub import AudioSegment
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect, url_for, render_template
 from flask_cors import CORS
 import audio_handler as ah
 app = Flask(__name__)
 CORS(app)
+
+
 
 beat = False
 
 import requests
 
 
-
 def generate_voice(lyrics):  # Accept lyrics as a parameter
     client = ElevenLabs(
-        api_key="sk_9bad27c0e85678bc95d3304cd21bb5eb7c1ec3d2f3199fcc", # Defaults to ELEVEN_API_KEY
+        api_key="sk_5ebfe0d699658a5de0dcc3cfe8b2394a0ac87a6ffbfd1500", # Defaults to ELEVEN_API_KEY
     )
 
     voice = client.clone(
@@ -25,14 +26,14 @@ def generate_voice(lyrics):  # Accept lyrics as a parameter
 
         name="clonedVoice",
         description="Your voice should be energetic and confident, like a hip-hop artist. Speak with a rhythmic flow, almost as if you're rapping, letting the words roll smoothly. Add emphasis to key words, giving your delivery swagger and personality, similar to Snoop Dogg's laid-back yet powerful style. Maintain a dynamic yet controlled pace, making the message hit hard. Feel the vibe and convey it with emotion, turning your speech into a performance that's lively, engaging, and impossible to ignore.", # Optional
-        files=["./uploads/recording.mp3"]
+        files=["./src/static/uploads/recording.mp3"]
        
 
     )
    
 
     audio = client.generate(text=lyrics, voice=voice)
-    with open("./uploads/final_audio.mp3", "wb") as file:
+    with open("./src/static/uploads/final_audio.mp3", "wb") as file:
         for chunk in audio:
             file.write(chunk)
         merge_audio()
@@ -46,19 +47,23 @@ def generate_voice(lyrics):  # Accept lyrics as a parameter
    
 
     url = "https://api.elevenlabs.io/v1/voices/" + voice.voice_id
-    headers = {"xi-api-key": "sk_9bad27c0e85678bc95d3304cd21bb5eb7c1ec3d2f3199fcc"}
+    headers = {"xi-api-key": "sk_5ebfe0d699658a5de0dcc3cfe8b2394a0ac87a6ffbfd1500"}
     response = requests.request("DELETE", url, headers=headers)
     print(123)
     print(response.text)
 
 def merge_audio():
     audio1 = AudioSegment.from_file("./src/Resources/beat1.mp3")
-    audio2 = AudioSegment.from_file("./uploads/final_audio.mp3")
+    audio2 = AudioSegment.from_file("./src/static/uploads/final_audio.mp3")
     # audio2 = AudioSegment.from_file("./src/speech.mp3")
     audio2.volume = 10
     audio1.volume = 1
     audio1 = audio1[:(audio2.duration_seconds * 1000)]
     final_audio = audio1.overlay(audio2)
-    final_audio.export("./uploads/merged_audio.mp3", format="mp3")
+    final_audio.export("./src/static/uploads/merged_audio.mp3", format="mp3")
     print("Audio merged successfully")
+    
+
+
+
 
